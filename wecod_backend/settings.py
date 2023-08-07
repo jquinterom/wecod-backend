549429a65985135faa166449a282e32459b6f5e4
jquinterom <jfqm7710@gmail.com>
2023-08-07 12:36:25 -0500

    Url para CORS

--- wecod_backend/settings.py
+++ wecod_backend/settings.py
@@ -140,7 +140,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Cors auth
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:5173"
+    "https://wecod-backend.onrender.com/"
 ]
 
 REST_FRAMEWORK = {
