5c98ad90bee28be046a794e2399ec0df2a6fa8aa
jquinterom <jfqm7710@gmail.com>
2023-08-14 15:20:28 -0500

    Models for custom weapon

--- weapons/admin.py
+++ weapons/admin.py
@@ -1,6 +1,29 @@
 from django.contrib import admin
-from .models import Weapon, Category
+from .models import (
+    Weapon,
+    Category,
+    Mozzle,
+    Barrel,
+    Optic,
+    Stock,
+    Perk,
+    Lazer,
+    Underbarrel,
+    Ammunition,
+    RearGrip,
+    CustomWeapon
+)
 
 # Register your models here.
-admin.site.register(Weapon)
 admin.site.register(Category)
+admin.site.register(Weapon)
+admin.site.register(Mozzle)
+admin.site.register(Barrel)
+admin.site.register(Optic)
+admin.site.register(Stock)
+admin.site.register(Perk)
+admin.site.register(Lazer)
+admin.site.register(Underbarrel)
+admin.site.register(Ammunition)
+admin.site.register(RearGrip)
+admin.site.register(CustomWeapon)
--- weapons/models.py
+++ weapons/models.py
@@ -16,7 +16,115 @@ class Category(models.Model):
 
 class Weapon(models.Model):
     name = models.CharField(max_length=200)
-    category = models.ForeignKey(Category, null=True, on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        Category, null=True, on_delete=models.CASCADE)
+    created_timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated_timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Mozzle(models.Model):
+    name = models.CharField(max_length=200)
+    created_timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated_timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Barrel(models.Model):
+    name = models.CharField(max_length=200)
+    created_timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated_timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Optic(models.Model):
+    name = models.CharField(max_length=200)
+    created_timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated_timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Stock(models.Model):
+    name = models.CharField(max_length=200)
+    created_timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated_timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Perk(models.Model):
+    name = models.CharField(max_length=200)
+    created_timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated_timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Lazer(models.Model):
+    name = models.CharField(max_length=200)
+    created_timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated_timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Underbarrel(models.Model):
+    name = models.CharField(max_length=200)
+    created_timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated_timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Ammunition(models.Model):
+    name = models.CharField(max_length=200)
+    created_timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated_timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class RearGrip(models.Model):
+    name = models.CharField(max_length=200)
+    created_timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated_timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class CustomWeapon(models.Model):
+    name = models.CharField(max_length=200, null=False, default="Uknown")
+    original_weapon = models.ForeignKey(
+        Weapon, null=False, on_delete=models.DO_NOTHING)
+    mozzle = models.ForeignKey(
+        Mozzle, null=True, on_delete=models.DO_NOTHING)
+    barrel = models.ForeignKey(
+        Barrel, null=True, on_delete=models.DO_NOTHING)
+    optic = models.ForeignKey(Optic, null=True, on_delete=models.DO_NOTHING)
+    stock = models.ForeignKey(Stock, null=True, on_delete=models.DO_NOTHING)
+    perk = models.ForeignKey(Perk, null=True, on_delete=models.DO_NOTHING)
+    lazer = models.ForeignKey(Lazer, null=True, on_delete=models.DO_NOTHING)
+    underbarrel = models.ForeignKey(
+        Underbarrel, null=True, on_delete=models.DO_NOTHING)
+    ammunition = models.ForeignKey(
+        Ammunition, null=True, on_delete=models.DO_NOTHING)
+    reargrip = models.ForeignKey(
+        RearGrip, null=True, blank=True, on_delete=models.DO_NOTHING)
+
     created_timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated_timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
 
