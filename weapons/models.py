9f37afd63558ff38ff5a44c993fb1e95c8c710eb
jquinterom <jfqm7710@gmail.com>
2023-08-17 08:31:05 -0500

    Name for CustomWeapon model

--- weapons/models.py
+++ weapons/models.py
@@ -88,15 +88,6 @@ class Underbarrel(models.Model):
         return self.name
 
 
-class Ammunition(models.Model):
-    name = models.CharField(max_length=200)
-    created_timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
-    updated_timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
-
-    def __str__(self) -> str:
-        return self.name
-
-
 class RearGrip(models.Model):
     name = models.CharField(max_length=200)
     created_timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
