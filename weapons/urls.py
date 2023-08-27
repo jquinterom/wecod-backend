081e389d989d09a6af88d9de843519ce2256e02e
jquinterom <jfqm7710@gmail.com>
2023-08-27 12:30:01 -0500

    Adding url to prefix

--- weapons/urls.py
+++ weapons/urls.py
@@ -7,15 +7,14 @@ router = routers.DefaultRouter()
 router.register(r'weapons', views.WeaponView, 'weapons')
 router.register(r'categories', views.CategoryView, 'categories')
 router.register(r'customWeapons', views.CustomWeaponView, 'customWeapons')
-router.register(r'rateCustomWeapons',
-                views.AverageRateCustomWeaponView, 'rateCustomWeapons')
 router.register(r'gameModes',
                 views.GameModesView, 'gameModes')
 
 urlpatterns = [
     path("", include(router.urls)),
-    path('customWeaponTwo/<int:id>/',
-         views.customWeaponTwoView.as_view(), name='customWeaponTwo')
 ]
 
-# urlpatterns = [path(r'^api/v1/', include(urlpatterns))]
+urlpatterns += [path('rateCustomWeapons/',
+                     views.AverageRateCustomWeaponView.as_view(), name='rateCustomWeapons'),
+                path('customWeaponTwo/<int:id>/',
+                     views.customWeaponTwoView.as_view(), name='customWeaponTwo')]
